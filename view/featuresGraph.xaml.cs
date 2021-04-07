d73b680cb23e110a1485944cb85056c60d9045a7
AvrahamBi <avrahambarilan@gmail.com>
2021-04-07 14:13:02 +0300

    Features Graph View

--- view/featuresGraph.xaml.cs
+++ view/featuresGraph.xaml.cs
@@ -16,23 +16,34 @@ using System.Windows.Shapes;
 namespace FlightSimulator2.view
 {
     /// <summary>
-    /// Interaction logic for featuresGraph.xaml
+    /// Interaction logic for FeaturesGraph.xaml
     /// </summary>
-    public partial class featuresGraph : Page
+    public partial class FeaturesGraph : UserControl
     {
-        public featuresGraph()
+
+        private string[] featuresArr = { "aileron", "elevator", "rudder", "flaps", "slats", "speedbrake", "throttle", "throttle", "engine-pump", "engine-pump", // flight features according to XML file
+            "electric-pump", "electric-pump", "external-power", "APU-generator", "latitude-deg", "longitude-deg", "altitude-ft", "roll-deg", "pitch-deg",
+            "heading-deg", "side-slip-deg", "airspeed-kt", "glideslope", "vertical-speed-fps", "airspeed-indicator_indicated-speed-kt", "altimeter_indicated-altitude-ft",
+            "altimeter_pressure-alt-ft", "attitude-indicator_indicated-pitch-deg", "attitude-indicator_indicated-roll-deg", "attitude-indicator_internal-pitch-deg",
+            "attitude-indicator_internal-roll-deg", "encoder_indicated-altitude-ft", "encoder_pressure-alt-ft", "gps_indicated-altitude-ft", "gps_indicated-ground-speed-kt",
+            "gps_indicated-vertical-speed", "indicated-heading-deg", "magnetic-compass_indicated-heading-deg", "slip-skid-ball_indicated-slip-skid",
+            "turn-indicator_indicated-turn-rate", "vertical-speed-indicator_indicated-speed-fpm", "engine_rpm"};
+
+
+        private void bindListBox()
+        {
+            featuresListBox.ItemsSource = featuresArr;
+        }
+
+        private void featuresListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        public FeaturesGraph()
         {
             InitializeComponent();
-        }
-
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
-        {
-
+            bindListBox();
         }
     }
 }
