6b6911f79eaf47aa4a11091811fce5ee5f7b1765
AvrahamBi <avrahambarilan@gmail.com>
2021-04-08 23:52:18 +0300

    story 6 update

--- view/FeaturesGraphV.xaml.cs
+++ view/FeaturesGraphV.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using FlightSimulator2.viewModel;
+using FlightSimulator2.model;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using OxyPlot;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,14 +23,21 @@ namespace FlightSimulator2.view
     /// </summary>
     public partial class FeaturesGraphV : UserControl
     {
+        private FeaturesGraphVM _viewModel;
+        public FeaturesGraphVM _ViewModel
+        {
+            get { return _ViewModel; }
+        }
         public FeaturesGraphV()
         {
             InitializeComponent();
+            _viewModel = new FeaturesGraphVM(new FeaturesGraphM());
+            DataContext = _viewModel;
         }
 
         private void featuresListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            // todo
         }
     }
 }
