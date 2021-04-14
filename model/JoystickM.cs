6ad0084481c2df4ea30b1c68e0380052a54e502c
Ron Harel <rontsohrl@gmail.com>
2021-04-14 10:31:50 +0300

    story 4 + 8

---
+++ model/JoystickM.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Threading;
+
+namespace FlightSimulator2.model
+{
+    class JoystickM : INotifyPropertyChanged
+    {
+        private bool disconnect;
+        private int elevator_idx, aileron_idx, throttle_idx, rudder_idx;
+        private double elevator, aileron, throttle, rudder;
+        public event PropertyChangedEventHandler PropertyChanged;
+        /*PlayerControlBarM control_bar;
+        Client client;*/
+
+        public JoystickM()
+        {
+            this.disconnect = false;
+            this.aileron_idx = 0;
+            this.elevator_idx = 1;
+            this.throttle_idx = 7;
+            this.rudder_idx = 2;
+        }
+        public double Elevator
+        {
+            get
+            {
+                return elevator;
+            }
+
+            set
+            {
+                elevator = value;
+                NotifyPropertyChanged("Elevator");
+            }
+
+        }
+
+        public double Aileron
+        {
+            get
+            {
+                return aileron;
+            }
+
+            set
+            {
+                aileron = value;
+                NotifyPropertyChanged("Aileron");
+            }
+
+        }
+
+        public double Throttle
+        {
+            get
+            {
+                return throttle;
+            }
+
+            set
+            {
+                throttle = value;
+                NotifyPropertyChanged("Throttle");
+            }
+
+        }
+
+        public double Rudder
+        {
+            get
+            {
+                return rudder;
+            }
+
+            set
+            {
+                elevator = value;
+                NotifyPropertyChanged("Rudder");
+            }
+
+        }
+
+        public void NotifyPropertyChanged(String propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        public void flightSpecifiecJoystickMode()
+        {
+            new Thread(delegate ()
+            {
+                while (disconnect == false)
+                {
+                    if (Client.client_instance.currentFlightState() == null) continue;
+                    string[] flightsInsturment;
+                    flightsInsturment = Client.client_instance.currentFlightState().Split(',');
+                    this.elevator = Convert.ToDouble(flightsInsturment[this.elevator_idx]);
+                    this.aileron = Convert.ToDouble(flightsInsturment[this.aileron_idx]);
+                    this.throttle = Convert.ToDouble(flightsInsturment[this.throttle_idx]);
+                    this.rudder = Convert.ToDouble(flightsInsturment[this.rudder_idx]);
+                    /*this.Flight_speed = Convert.ToDouble(flightsInsturment[speed_index]);
+                    this.Altimeter = Convert.ToDouble(flightsInsturment[altimeter_index]);
+                    this.Head_deg = Convert.ToDouble(flightsInsturment[headdeg_index]);
+                    this.Roll = flightsInsturment[roll_index];
+                    this.Yaw = flightsInsturment[yaw_index];
+                    this.Pitch = flightsInsturment[pitch_index];
+*/                }
+            }).Start();
+        }
+    }
+}
---
+++ viewModel/RegresionLineVM.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using FlightSimulator2.model;
+using System.ComponentModel;
+using OxyPlot.Annotations;
+using OxyPlot;
+using OxyPlot.Axes;
+using FlightSimulator2.viewModel;
+
+namespace FlightSimulator2.viewModel
+{
+    class RegresionLineVM
+    {
+        RegresionLineM rl;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public RegresionLineVM(RegresionLineM new_reg_line)
+        {
+            this.rl = new_reg_line;
+            this.rl.PropertyChanged += delegate (object sender, PropertyChangedEventArgs e) {
+                NotifyPropertyChanged("VM_" + e.PropertyName);
+            };
+        }
+
+        public List<DataPoint> VM_Points
+        {
+            get
+            {
+                return this.rl.Points;
+            }
+
+            set
+            {
+                this.rl.Points = value;
+                NotifyPropertyChanged("VM_Points");
+            }
+        }
+
+        public List<DataPoint> VM_LineByPoints
+        {
+            get
+            {
+                return this.rl.LineByPoints;
+            }
+            
+            set
+            {
+                this.rl.LineByPoints = value;
+                NotifyPropertyChanged(nameof(VM_LineByPoints));
+            }
+        }
+
+        public Line VM_Reg_line
+        {
+            get
+            {
+                return this.rl.Reg_line;
+            }
+
+            set
+            {
+                this.rl.Reg_line = value;
+                NotifyPropertyChanged("VM_Reg_line");
+            }
+        }
+
+       /* public void FindRegLine()
+        {
+            this.rl.Reg_line.linear_reg(this.X_feature_list, this.Y_feature_list, this.X_feature_list.Count());
+        }*/
+        public List<DataPoint> X_feature_list
+        {
+            get
+            {
+                return this.rl.X_feature_list;
+            }
+
+            set
+            {
+                this.rl.X_feature_list = value;
+                NotifyPropertyChanged("VM_X_feature_list");
+            }
+        }
+
+        public List<DataPoint> Y_feature_list
+        {
+            get
+            {
+                return this.rl.Y_feature_list;
+            }
+
+            set
+            {
+                this.rl.Y_feature_list = value;
+                NotifyPropertyChanged("VM_Y_feature_list");
+            }
+        }
+        public string X_feature
+        {
+            get
+            {
+                return this.rl.X_feature;
+            }
+
+            set
+            {
+                this.rl.X_feature = value;
+                NotifyPropertyChanged("VM_X_feature");
+            }
+        }
+
+        public string Y_feature
+        {
+            get
+            {
+                return this.rl.Y_feature;
+            }
+
+            set
+            {
+                this.rl.Y_feature = value;
+                NotifyPropertyChanged("VM_Y_feature");
+            }
+        }
+
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+    }
+}
